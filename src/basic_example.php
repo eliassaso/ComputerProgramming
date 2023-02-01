78ffeb63f5fdb71e243d26f7234db4c3ad1ccae7
Elias <eliassaso@gmail.com>
2023-01-31 20:24:33 -0600

    Syntax

--- src/basic_example.php
+++ src/basic_example.php
@@ -1,5 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-phpinfo();
+<body>
 
-?>
+    <h1>PHP</h1>
+
+    <?php
+
+    print "<h2>1 - Syntax</h2>";
+    print "<br>";
+    print "Hola Mundo";
+    print "<br>";
+    echo "Hola Mundo con echo";
+    print "<br>";
+    ECHO "Hola Mundo con ECHO";
+    print "<br>";
+    echo "hola ", "mundo";
+    // print "hola",
+
+    print "<h2>*****</h2>";
+
+    print "<h2>2 - Variables and types</h2>";
+
+    ?>
+
+</body>
+
+</html>
